c451997dbd1fb45bb57e2f67cef2d3850fede343
Diogo Perin <diogopers@gmail.com>
2017-02-21 14:21:09 -0300

    better routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -16,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :photo, :photo_cache)
   end
 end
