5b36a36c98e1aeb9f1d3b95328607f9004a9e672
Madalena Pessanha <madalena.lpessanha@gmail.com>
2017-02-21 17:25:29 -0300

    calendar working for safari too

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 
+
 group :development, :test do
   gem 'binding_of_caller'
   gem 'better_errors'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+  $(document).ready(function() {
+    $(".datepicker").datepicker();
+  });
