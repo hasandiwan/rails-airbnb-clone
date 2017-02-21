87e1f29e26f1fda3f27eced4d319222c567187f0
Diogo Perin <diogopers@gmail.com>
2017-02-21 16:29:28 -0300

    better-update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,29 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-
-    redirect_to user_path(@user)
+    current_user.update(user_params)
+    redirect_to user_path
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :first_name,
+                                       :street_address, :city,
+                                       :zipcode, :phone, :about,
+                                       :reviews_attributes => [:content, :rating])
+  end
+
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
+
+  # def update
+  #   @user = User.find(params[:id])
+  #   @user.update(user_params)
+
+  #   redirect_to user_path(@user)
+  # end
 
 end
