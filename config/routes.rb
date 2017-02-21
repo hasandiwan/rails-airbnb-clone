f19d84ef629ba572842a5b87f4cd1cbf8d1c80a5
Diogo Perin <diogopers@gmail.com>
2017-02-21 14:12:17 -0300

    better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
 
-  get 'bookings/create'
+  resources :users, only: [:show, :edit, :update]
 
-  get 'sitters/index'
+  resources :bookings, only: [:new, :create]
 
-  get 'users/show'
+  resources :sitters, only: [:index]
 
-  get 'users/edit'
+  # get 'sitters/index'
 
-  get 'users/update'
+  # get 'users/show'
+
+  # get 'users/edit'
+
+  # get 'users/update'
 
 
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
